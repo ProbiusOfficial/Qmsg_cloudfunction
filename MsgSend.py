aa530031d0d5afdae5f1af462d328bb7ec8c4a9f
ProbiusOfficial <41804496+ProbiusOfficial@users.noreply.github.com>
2021-07-21 18:12:54 +0800

    Update MsgSend.py

--- MsgSend.py
+++ MsgSend.py
@@ -1,6 +1,6 @@
 import requests
 # qmsg get the key
-host="https://qmsg.zendee.cn/send/24130fa4e009ee90f6bb4895c5357ab5"
+host="https://qmsg.zendee.cn/send/Your_Key"
 #Module of push，need the QQ Num and The message you need push
 def postData(qq,msg):
     data={
